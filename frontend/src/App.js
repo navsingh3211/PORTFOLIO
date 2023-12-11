a745b58f8fdc80d058b1f2d016eed79b3785931e
navsingh3211 <navsingh3211@gmail.com>
2023-12-11 16:52:21 +0530

    basic component and designs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,32 @@
+/* eslint-disable no-unused-vars */
 
-import './App.css';
+import "./App.css";
+import {BrowserRouter as Router,Route,Routes} from "react-router-dom";
+// import Home from "./components/Home/Home";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Nav from "./components/Nav/Nav";
+import Myprojects from "./components/MyProjects/MyProjects";
+import About from "./components/About/About";
+import Contacts from "./components/Contacts/Contacts";
+import Experience from "./components/Experience/Experience";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
-  );
+    
+    <>
+        <Header/>
+        <Nav/>
+        <About/>
+        <Experience/>
+        <Myprojects/>
+        <Contacts/>
+        <Footer/>
+
+    </>
+  )
 }
+
+
 
 export default App;
---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './header.css'
+import CTA from './CTA'
+import me from '../../assets/me.jpeg'
+import HeaderSocials from './HeaderSocials'
+
+const Header = () => {
+  return (
+      <header id='home'>
+
+        <div className="container header__container">
+
+          <h5>Hello, I'm</h5>
+          <h1>Navneet Singh</h1>
+          <h5 className="text-light">Multi-stack developer</h5>
+          <CTA/>
+          <HeaderSocials/>
+
+          <div className="me">
+
+            <img src={me} alt='me'/>
+          </div>
+
+          <a href="#contacts" className='scroll__down'>Scroll Down</a>
+        </div>
+      </header>
+  )
+}
+
+export default Header
